f6a4b3a4bb619c757eb15befcefb495040997717
BorzoiDen <borozenetsdn@gmail.com>
2022-12-03 17:05:51 +0700

    js 2.0 lessons 1-3 end

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-1
+//const input = document.querySelector('.input');
+//const input2 = document.querySelector('.input2');
+const select = document.querySelector('.select');
+const select2 = document.querySelector('.select2');
+const select3 = document.querySelector('.select3');
+const out = document.querySelector('.out');
+
+function f15(){
+  switch(select3.value){
+    case '&&':
+      out.innerHTML = (+select.value && +select2.value);
+      break;
+    case '||':
+      out.innerHTML = (+select.value || +select2.value);
+      break;
+  }
+}
+document.querySelector('.button').onclick = f15;
