c341db1f9931c163d83816769ca6d26267851321
BorzoiDen <borozenetsdn@gmail.com>
2022-12-04 15:49:13 +0700

    js lesson 5 start

--- script.js
+++ script.js
@@ -4,16 +4,15 @@
 //const input2 =  document.querySelector('#inp2');
 //const select2 = document.querySelector('.select2');
 //const select3 = document.querySelector('.select3');
-const input =  document.querySelector('.input-t');
-const input2 =  document.querySelector('.input-p');
-const out = document.querySelector('.out');
-const formb = document.querySelector('.formb');
 //const select = document.querySelector('.select');
-function f20(e) {
-  e.preventDefault(); // чтобы форма не перезагружала страницу!!!!
-  let form = document.querySelector('.form');
-  out.innerHTML = form.elements['username'].value;
-  out.innerHTML += ' ';
-  out.innerHTML += form.elements['userpass'].value;
+//const input =  document.querySelector('.input-t');
+//const input2 =  document.querySelector('.input-p');
+//const out = document.querySelector('.out');
+
+function t1() {
+  const out = document.querySelector('.out');
+  let total = '';
+  for(let i = 1; i <= 50; )
 }
-document.querySelector('.formb').onclick = f20;
+
+document.querySelector('.b').onclick = t1;
