f152adaef6a9aa43ab79a275f3f99de28b3d3982
BorzoiDen <borozenetsdn@gmail.com>
2022-12-04 17:15:31 +0700

    js lesson 6 startt

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//const input = document.querySelector('.input');
+
 //const input2 = document.querySelector('.input2');
 //const input =  document.querySelector('#inp');
 //const input2 =  document.querySelector('#inp2');
@@ -8,11 +8,12 @@
 //const input =  document.querySelector('.input-t');
 //const input2 =  document.querySelector('.input-p');
 //const out = document.querySelector('.out');
-
-function t1() {
-  const out = document.querySelector('.out');
-  let total = '';
-  for(let i = 1; i <= 50; )
-}
-
-document.querySelector('.b').onclick = t1;
+const input = document.querySelector('.input');
+const input2 = document.querySelector('.input2');
+const out = document.querySelector('.out');
+function t15() {
+  for(let i = 0; i <= 10; i++){
+      out.innerHTML += (10 - i) + ' ' + i + ' '; 
+    }
+  }
+document.querySelector('.b').onclick = t15;
