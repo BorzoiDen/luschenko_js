cf3e57a095511fca16b3060da4bf8fe586aa70be
BorzoiDen <borozenetsdn@gmail.com>
2022-12-04 11:53:43 +0700

    js progress 04.12.2022

--- script.js
+++ script.js
@@ -3,16 +3,6 @@
 const select = document.querySelector('.select');
 const select2 = document.querySelector('.select2');
 const select3 = document.querySelector('.select3');
-const out = document.querySelector('.out');
 
-function f15(){
-  switch(select3.value){
-    case '&&':
-      out.innerHTML = (+select.value && +select2.value);
-      break;
-    case '||':
-      out.innerHTML = (+select.value || +select2.value);
-      break;
-  }
-}
-document.querySelector('.button').onclick = f15;
+const out = document.querySelector('.out');
+const input =  document.querySelector('#inp');
