4c308bfde294aa7926daf00f99b617c1c83a4fd1
BorzoiDen <borozenetsdn@gmail.com>
2022-12-12 21:51:55 +0700

    js 12 12 2022 progress

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
+let ar19 = [15, 424, 313, 78, 241, 4, 45, 67];
 
-//const input2 = document.querySelector('.input2');
-//const input =  document.querySelector('#inp');
-//const input2 =  document.querySelector('#inp2');
-//const select2 = document.querySelector('.select2');
-//const select3 = document.querySelector('.select3');
-//const select = document.querySelector('.select');
-//const input =  document.querySelector('.input-t');
-//const input2 =  document.querySelector('.input-p');
-//const out = document.querySelector('.out');
-//const input = document.querySelector('.input');
-//const input2 = document.querySelector('.input2');
-function f20() {
-  const out = document.querySelector('.out');
-  out.setAttribute('title', 'go');
-} 
-document.querySelector('.b').onclick = f20;
+function f19() {
+  let out = document.querySelector('.out'); 
+  let min = ar19[0];
+  for(let i = 0; i < ar19.length; i++){
+    if(ar19[i] < min){
+      min = ar19[i];
+    }
+  }
+  out.innerHTML = min;
+}
+document.querySelector('.b').onclick = f19;
